6ff919ac28d847b98cb7585831281edd6dd0cb12
Luana <luanaff-2008@hotmail.com>
2022-08-17 16:05:37 -0300

    feat: add test model search all and search one car

--- src/tests/unit/models/model.test.ts
+++ src/tests/unit/models/model.test.ts
@@ -15,6 +15,8 @@ describe('Car Model', () => {
 
 	before(() => {
 		sinon.stub(Model, 'create').resolves(carMockWithId);
+        sinon.stub(Model, 'find').resolves(allCarsMockWithId);
+        sinon.stub(Model, 'findOne').resolves(carMockWithId);
 	});
 
 	after(() => {
@@ -25,6 +27,20 @@ describe('Car Model', () => {
 		it('successfully created a car', async () => {
 			const newCar = await carsModel.create(carMockCreate);
 			expect(newCar).to.be.deep.equal(carMockWithId);
+		});
+	});
+
+    describe('searching all cars', () => {
+		it('successfully found all cars', async () => {
+			const carsFound = await carsModel.read();
+			expect(carsFound).to.be.deep.equal(allCarsMockWithId);
+		});
+	});
+
+    describe('searching a Car', () => {
+		it('successfully found', async () => {
+			const carFound = await carsModel.readOne("62fd3403e8f068104275d3ec");
+			expect(carFound).to.be.deep.equal(carMockWithId);
 		});
 	});
 });
